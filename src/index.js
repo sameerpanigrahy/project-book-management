0e3d2ec439194aac872a2d8ac50dc5b4f41a1f73
myselfaditya <adityasuhaspatil@gmail.com>
2022-09-20 12:59:00 +0530

    first work

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const route = require('./routes/route.js');
+const mongoose = require('mongoose');
+const app = express();
+const PORT=process.env.PORT||3000
+
+app.use(express.json());
+
+
+mongoose.connect("mongodb+srv://princekumar-2:ZNmRphvSvecrtzBV@cluster0.yibukmr.mongodb.net/Group-53?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"),err => console.log(err))
+
+app.use('/', route);
+
+
+app.listen(  PORT, function () {
+    console.log('Express app running on port ' + PORT)
+});
---
+++ src/routes/route.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const {createUser} = require('../controllers/userController')
+
+router.post('/authors',createUser)
